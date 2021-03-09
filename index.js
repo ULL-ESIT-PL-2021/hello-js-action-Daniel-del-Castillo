4deecf09e4c0b92352a7fbdecca9f06f4db51078
Daniel-del-Castillo <alu0101225548@ull.edu.es>
2021-03-09 15:41:49 +0000

    Add a differenct action

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-/**
- * Language processors
- * Universidad de La Laguna 2020/2021
- * @author Daniel del Castillo 
- * @since 08/03/2021
- * @desc A simple Github Action
- */
-
 const core = require('@actions/core');
-const github = require('@actions/github');
+const wait = require('./wait');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+
+// most @actions toolkit packages have async methods
+async function run() {
+  try {
+    const ms = core.getInput('milliseconds');
+    const name = core.getInput('name');
+    core.info(`Waiting ${ms} milliseconds ...`);
+
+    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    await wait(parseInt(ms));
+    core.info(`${name} seal of approval granted`);
+    core.info((new Date()).toTimeString());
+
+    core.setOutput('time', new Date().toTimeString());
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
